959355f7cb966f66e265cfe688e6fe94962db08c
Peter Offem <nescooffem@gmail.com>
2023-12-16 12:22:54 +0100

    test stuff

--- authentication-service/cmd/api/setup_test.go
+++ authentication-service/cmd/api/setup_test.go
@@ -3,8 +3,15 @@ package main
 import (
 	"os"
 	"testing"
+
+	"github.com/Offem-Peter/go-microservices/authentication-service/data"
 )
 
+var testApp Config
+
 func TestMain(m *testing.M) {
+
+	repo := data.NewPostgresRepository(nil)
+	testApp.Repo = repo
 	os.Exit(m.Run())
 }
